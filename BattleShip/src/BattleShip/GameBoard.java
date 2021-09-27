a591fa7d45ef5ceb7e0acfa2a1ce0a3c6c6c78fe
gerald.hills <geraldhillsjr.gmail.com>
2021-09-27 18:42:07 -0400

    Added ships to board

--- BattleShip/src/BattleShip/GameBoard.java
+++ BattleShip/src/BattleShip/GameBoard.java
@@ -1,4 +1,5 @@
 package BattleShip;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class GameBoard
@@ -69,7 +70,49 @@ public class GameBoard
 	//Returns true on successful addition; false, otherwise
 	public boolean addShip( Ship s , Position sternLocation, HEADING bowDirection )
 	{
-		return false;
+		ArrayList< Position > p = new ArrayList<>();
+		p.add(sternLocation);
+
+		if(bowDirection.equals( HEADING.EAST ) )
+			for (int i = 1; i < s.getLength(); ++i )
+				p.add( new Position(sternLocation.x + i, sternLocation.y) );
+		else if (bowDirection.equals( HEADING.SOUTH ) )
+			for( int i = 1; i< s.getLength(); i++)
+				p.add( new Position( sternLocation.x, sternLocation.y + i) );
+		else if (bowDirection.equals( HEADING.WEST) )
+			for (int i = 1; i < s.getLength(); i++ )
+				p.add( new Position(sternLocation.x -i, sternLocation.y) );
+		else if( bowDirection.equals( HEADING.NORTH ) )
+			for( int i = 1; i < s.getLength(); ++i )
+				p.add( new Position( sternLocation.x, sternLocation.y - i ) );
+
+		//valid candidate coordinates to ensure they reside on board
+		for( Position c : p)
+		{
+			if( c.x < 0 || c.y < 0 )
+				return false;
+			if( c.x > colCount - 1 || c.y > rowCount - 1 )
+				return false;
+		}
+
+		//ensure the candidate positions do not collide w/ existing ship
+		for( Position c : p )
+		{
+			if( this.cells.get(c.y).get(c.x).getShip() != null ) // a ship at this location has been found
+				return false; // a collision has occurred
+		}
+
+		ArrayList< Cell > shipPos = new ArrayList<>();
+		for( Position c : p )
+		{
+			Cell cell = this.cells.get(c.y).get(c.x);
+			cell.setShip(s);
+			shipPos.add( cell );
+		}
+		s.setPosition( shipPos );
+		this.myShips.add( s );
+
+		return true;
 	}
 	
 	//Returns A reference to a ship, if that ship was struck by a missle.
